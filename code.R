f26b2bf02217b6222f8475017eef77e3cc398dc0
Jacob Anhøj <jacob@anhoej.net>
2025-07-02 09:28:07 +0200

    Polish.

--- code.R
+++ code.R
@@ -9,12 +9,12 @@ bp <- c(169, 172, 175, 174, 161, 142,
         157, 183, 177, 171, 185, 176,
         181, 174)
 
-# build-a-plot
+# build-a-plot with base R
 cl  <- mean(bp)
 amr <- mean(abs(diff(bp)))
 s   <- amr / 1.128
-lcl <- cl - 3 * s
-ucl <- cl + 3 * s
+lcl <- cl - 3 * s       # cl - 2.66 * amr
+ucl <- cl + 3 * s       # cl + 2.66 * amr
 
 plot(bp, type = 'o', ylim = range(bp, lcl, ucl))
 abline(h = c(lcl, cl, ucl))
@@ -23,6 +23,12 @@ abline(h = c(lcl, cl, ucl))
 pbc(bp)
 
 pbc(bp, chart = 'i')
+
+pbc(bp, 
+    chart = 'i',
+    title = 'Systolic blood pressure', 
+    ylab  = 'mm Hg', 
+    xlab  = 'Day')
 
 ?pbc
 
@@ -35,17 +41,17 @@ pbc(month, avg_delay,
 
 # I' chart without denominator
 pbc(month, avg_delay,
-    data = csection,
+    data  = csection,
     chart = 'i')
 
 # I' chart with denominator (forgetting to multiply)
 pbc(month, avg_delay, n,
-    data = csection,
+    data  = csection,
     chart = 'i')
 
 # with aggregated measurements, remember to multiply numerator by denominator
 pbc(month, avg_delay * n, n,
-    data = csection,
+    data  = csection,
     chart = 'i')
 
 # HbA1c in children with diabetes ----
@@ -57,12 +63,12 @@ pbc(month, avg_hba1c,
 
 # I' chart without denominator
 pbc(month, avg_hba1c,
-    data = hba1c,
+    data  = hba1c,
     chart = 'i')
 
 # I' chart with denominator
 pbc(month, avg_hba1c * n, n,
-    data = hba1c,
+    data  = hba1c,
     chart = 'i')
 
 # On-time CT scan ----
@@ -74,7 +80,7 @@ pbc(month, ontime, cases,
 
 # I' chart of proportions
 pbc(month, ontime, cases,
-    data = ontime_ct,
+    data  = ontime_ct,
     chart = 'i')
 
 # Hospital acquired Clostridioides difficile infections ----
@@ -86,29 +92,29 @@ pbc(month, n,
 
 # I' chart with freeze after month 24
 pbc(month, n,
-    data = cdi,
-    chart = 'i',
+    data   = cdi,
+    chart  = 'i',
     freeze = 24)
 
 # split chart
 pbc(month, n,
-    data = cdi,
+    data  = cdi,
     chart = 'i',
     split = 24)
 
 # exclude freak data point (20)
 pbc(month, n,
-    data = cdi,
-    chart = 'i',
-    split = 24,
+    data    = cdi,
+    chart   = 'i',
+    split   = 24,
     exclude = 20)
 
 # plot rates per 10000 days
 pbc(month, n, days,
-    data = cdi,
-    chart = 'i',
-    split = 24,
-    exclude = 20,
+    data     = cdi,
+    chart    = 'i',
+    split    = 24,
+    exclude  = 20,
     multiply = 10000)
 
 # Bacteremia 30-day mortality ----
@@ -116,27 +122,29 @@ View(bacteremia_mortality)
 
 # I' chart faceted by hospital
 pbc(month, deaths, cases,
-    data = bacteremia_mortality,
+    data  = bacteremia_mortality,
     facet = hospital,
     chart = 'i')
 
 # formatting y axis as percentages
 pbc(month, deaths, cases,
-    data = bacteremia_mortality,
+    data  = bacteremia_mortality,
     facet = hospital,
     chart = 'i',
-    ypct = TRUE)
+    ypct  = TRUE)
 
 # free y axes
 pbc(month, deaths, cases,
-    data = bacteremia_mortality,
-    facet = hospital,
-    chart = 'i',
-    ypct = TRUE,
+    data   = bacteremia_mortality,
+    facet  = hospital,
+    chart  = 'i',
+    ypct   = TRUE,
     yfixed = FALSE)
 
 # Structure and summary of a pbc object ----
-p <- pbc(bp, chart = 'i', plot = FALSE)
+p <- pbc(bp, 
+         chart = 'i',
+         plot  = FALSE)
 
 str(p)
 

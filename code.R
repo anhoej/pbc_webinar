e5eb283fc98db75a1cc7795f34e761dcb7ef7a1b
Jacob Anhøj <jacob@anhoej.net>
2025-08-06 10:24:24 +0200

    Update code.R

--- code.R
+++ code.R
@@ -13,13 +13,13 @@ bp <- c(169, 172, 175, 174, 161, 142,
 cl  <- mean(bp)
 amr <- mean(abs(diff(bp)))
 s   <- amr / 1.128
-lcl <- cl - 3 * s       # cl - 2.66 * amr
-ucl <- cl + 3 * s       # cl + 2.66 * amr
+lcl <- cl - 3 * s
+ucl <- cl + 3 * s
 
 plot(bp, type = 'o', ylim = range(bp, lcl, ucl))
 abline(h = c(lcl, cl, ucl))
 
-# introducing pbcharts ----
+# Introducing pbcharts ----
 # run chart
 pbc(bp)
 
@@ -27,21 +27,17 @@ pbc(bp)
 pbc(bp, chart = 'i')
 
 # add titles
-pbc(bp, 
+pbc(bp,
     chart = 'i',
-    title = 'Systolic blood pressure', 
-    ylab  = 'mm Hg', 
+    title = 'Systolic blood pressure',
+    ylab  = 'mm Hg',
     xlab  = 'Day')
 
 # help
 ?pbc
 
-# on-time CT scan ----
+# On-time CT scan ----
 View(ontime_ct)
-
-# run chart of proportions
-pbc(month, ontime, cases,
-    data = ontime_ct)
 
 # I' chart of proportions
 pbc(month, ontime, cases,
@@ -57,10 +53,6 @@ pbc(month, ontime, cases,
 # HbA1c in children with diabetes ----
 View(hba1c)
 
-# run chart
-pbc(month, avg_hba1c,
-    data = hba1c)
-
 # I' chart without denominator
 pbc(month, avg_hba1c,
     data  = hba1c,
@@ -71,7 +63,7 @@ pbc(month, avg_hba1c, n,
     data  = hba1c,
     chart = 'i')
 
-# with aggregated measurements, remember to multiply numerator by denominator
+# with aggregated measurements, multiply numerator by denominator
 pbc(month, avg_hba1c * n, n,
     data  = hba1c,
     chart = 'i')
@@ -81,7 +73,7 @@ View(cdi)
 
 # I' chart of counts
 pbc(month, n,
-    data = cdi,
+    data  = cdi,
     chart = 'i')
 
 # I' chart with freeze after month 24
@@ -117,21 +109,21 @@ View(bacteremia_mortality)
 # I' chart faceted by hospital
 pbc(month, deaths, cases,
     data  = bacteremia_mortality,
-    facet = hospital,
-    chart = 'i')
+    chart = 'i',
+    facet = hospital)
 
-# censor y axis at zero
+# format y axis as percentage
 pbc(month, deaths, cases,
     data  = bacteremia_mortality,
-    facet = hospital,
     chart = 'i',
-    ylim  = c(0, NA))
+    facet = hospital,
+    ypct  = TRUE)
 
 # free y axes
 pbc(month, deaths, cases,
     data   = bacteremia_mortality,
-    facet  = hospital,
     chart  = 'i',
+    facet  = hospital,
     ypct   = TRUE,
     yfixed = FALSE)
 
@@ -139,18 +131,16 @@ pbc(month, deaths, cases,
 dev.off()
 p <- pbc(month, deaths, cases,
          data   = bacteremia_mortality,
-         facet  = hospital,
          chart  = 'i',
+         facet  = hospital,
          ypct   = TRUE,
          yfixed = FALSE,
          plot   = FALSE)
 
 str(p)
-
+p$title
 p$data
-
 summary(p)
-
 plot(p)
 
 # An interactive dashboard of avoidable hospitalisations ----
